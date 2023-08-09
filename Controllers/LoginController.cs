4d283e4babc5b247f3f283e5bb49814d9de7224b
minhtamDeveloper <minhtamceo2@gmail.com>
2023-08-09 12:56:51 +0700

    fisrt data

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PlantNestBackEnd.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PlantNestBackEnd.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private static Account account = new Account();
+    private IConfiguration configuration;
+    public LoginController(IConfiguration configuration) { 
+        this.configuration = configuration; 
+    }
+
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [HttpPost("login")]
+    public IActionResult login(Account account)
+    {
+        ;
+        return Ok(CreateToken(account.Username, account.FullName, account.Role));
+    }
+    private string CreateToken(string username,string name, string role)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier,username),
+            new Claim(ClaimTypes.Name,name),
+            new Claim(ClaimTypes.Role,role)
+        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value!));
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var token = new JwtSecurityToken(
+            claims:claims,
+            expires:DateTime.Now.AddDays(1),
+            signingCredentials: cred
+            );
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        return jwt;
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace PlantNestBackEnd.Models;
+
+public class Account
+{
+    public string Username { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
